bfa5d0ac8dc109984aca185929ceeb2068e867e4
Sohan Fernando <99402183+IT21314360@users.noreply.github.com>
2024-02-23 17:55:34 +0530

    Commit

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -34,7 +34,7 @@ const ProductDisplay = (props) => {
                 <div className="productdisplay-right-price-old">${product.old_price}</div>
                 <div className="productdisplay-right-price-new">${product.new_price}</div>
             </div>
-            <div className="productdsiplay-right-description">
+            <div className="productdisplay-right-description">
                 A lightweight, usually knitted, pullover shirt, close-fitting
             </div>
             <div className="productdisplay-right-size">
